ffe611cd5c6b2fd0b9ae459d686876a524c384f1
James Osborne <38009557+josborne-apilake@users.noreply.github.com>
2023-09-05 06:31:47 -0600

    Create TOU.sh
    Shell script to check if current day is not a public holiday, then execute miner and system shutdown

---
+++ TOU.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+holidays=("2023-07-04" "2023-09-04" "2023-11-23" "2023-12-25" "2024-01-01")
+today=`date -I`
+sleep=21600
+awake=`date -d "+$((sleep/3600)) hours"`
+
+if [[ ! " ${holidays[*]} " =~ " ${today} " ]]; then
+   logger "Today is TOU day. Stopping minner and rebooting $awake"
+   miner stop && sreboot wakealarm $sleep miner stop && sreboot wakealarm $sleep
+fi
